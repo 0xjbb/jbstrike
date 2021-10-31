06a31540b910c1549f59fcec2e1cfb825196dc5b
jb <jbork12@pm.me>
2021-10-31 09:08:52 +0000

    code

---
+++ server/generate_configs.py
@@ -0,0 +1,23 @@
+# this file will generate config.h and config.yaml for custom shit
+
+cpp_template = """
+
+
+
+"""
+
+json_template = """
+
+
+"""
+
+
+def randomString():
+    return ""
+
+def generate():
+    return ""
+
+
+if __name__ == "main":
+    generate()
