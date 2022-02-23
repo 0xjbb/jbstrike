78b5a9ed2d52595e03f0562fec9c87183990bec8
jb <jbork12@pm.me>
2022-02-23 21:10:56 +0000

    more code

--- teamserver/listeners.go
+++ teamserver/listeners.go
@@ -1,29 +1,49 @@
 package main
 
+import "net/http"
+
 type Listener struct {
 	ListenPort int
 	gConfig    Config
+	s          *http.Server
 }
 
 func (list *Listener) Listen() {
+	router := mux.NewRouter()
+
+	router.PathPrefix("/agent/{uuid}/{function}").HandlerFunc(HandleAgentHTTPRequests())
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":",
+	}
+
+	go srv.ListenAndServe()
 
 }
 
-func HandleAgentHTTPRequests(route string) {
+func HandleAgentHTTPRequests() func(w http.ResponseWriter, r *http.Request) {
 	//Route will be the requested URL query string
 	// All strings will be stored in the config.json to make it easier to change
-	switch route {
-	case gConfig.Listener.CheckInCmd:
-		//checkin()
-		break
-	case gConfig.Listener.DownloadCmd:
-		break
-	case gConfig.Listener.UploadCmd:
-		break
-	case gConfig.Listener.ExecCmd:
-		break
-	case gConfig.Listener.ExecAssemblyCmd:
-		break
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
 
+		uuid := vars["uuid"]
+		route := vars["function"]
+
+		switch route {
+		case gConfig.Listener.CheckInCmd:
+			//checkin()
+			break
+		case gConfig.Listener.DownloadCmd:
+			break
+		case gConfig.Listener.UploadCmd:
+			break
+		case gConfig.Listener.ExecCmd:
+			break
+		case gConfig.Listener.ExecAssemblyCmd:
+			break
+
+		}
 	}
 }
