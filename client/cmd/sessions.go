addc29b77c152d8d9cffe5b8e89d0a7ff52cc73a
jb <jbork12@pm.me>
2022-03-04 17:44:22 +0000

    lol ;

--- client/cmd/sessions.go
+++ client/cmd/sessions.go
@@ -1,1 +1,18 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/desertbit/grumble"
+)
+
+func init() {
+	App.AddCommand(&grumble.Command{
+		Name: "sessions",
+		Help: "List current sessions",
+		Run: func(c *grumble.Context) error {
+			fmt.Println("This is a test.")
+			return nil
+		},
+	})
+
+}
